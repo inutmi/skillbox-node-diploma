2cdbbb7670a632db9dbd11b73351694d60df7eb8
Александр Петухов <6and9@mail.ru>
2025-06-20 19:29:50 +0300

    file deleted

--- frontend-src/App.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-  import Router from "svelte-spa-router";
-  import Main from "./Main.svelte";
-  const routes = new Map();
-  routes.set(/^\/(.*)?/, Main);
-</script>
-
-<Router {routes} />
